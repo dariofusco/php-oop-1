b88fece5123ad390c5b081c7e0ce6b96b1ce9fb4
dariofusco <fdario@hotmail.it>
2023-09-19 14:30:26 +0200

    definisce metodo

--- Movie.php
+++ Movie.php
@@ -1,16 +1,24 @@
 <?php
 
-class Movie {
+class Movie
+{
     public $title;
+    public $genre;
     public $year;
-    
-    public function __construct($_title, $_year)
+
+    public function __construct($_title, $_genre, $_year)
     {
         $this->title = $_title;
+        $this->genre = $_genre;
         $this->year = $_year;
+    }
+
+    public function getFullDescription()
+    {
+        return 'Titolo: ' . $this->title . ', Genere: ' . $this->genre . ', Anno: ' . $this->year;
     }
 }
 
-$signoreDegliAnelli = new Movie('Il Signore Degli Anelli', '2002');
+$signoreDegliAnelli = new Movie('Il Signore Degli Anelli', 'Fantasy', '2002');
 
 ?>
--- index.php
+++ index.php
@@ -4,4 +4,6 @@ require_once __DIR__ . '/Movie.php';
 
 var_dump($signoreDegliAnelli);
 
+echo $signoreDegliAnelli->getFullDescription();
+
 ?>
